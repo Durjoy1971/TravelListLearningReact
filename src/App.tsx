80c34d94c631987dfcf1bbdee4be92a34ffd180a
Durjoy1971 <durjoybarua1971@gmail.com>
2024-09-23 21:27:56 +0600

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const HeaderDesign = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      🏝️ Far Away 🧳
+    </h1>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+    <HeaderDesign/>
+    <HeaderDesign/>
+    <HeaderDesign/>
+    <HeaderDesign/>
+    </div>
+  )
+}
